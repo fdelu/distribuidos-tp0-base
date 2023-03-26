40746639d389ac353add382726329a0c89862c04
fdelu <fdeluca@fi.uba.ar>
2023-03-26 03:56:30 -0300

    Add some comments to the client code

--- client/common/agency.go
+++ client/common/agency.go
@@ -26,6 +26,7 @@ func NewAgency(config Config) *Agency {
 	return &Agency{config: config, stopped: stopped}
 }
 
+// Exits the program. Closes the client socket and the bets file
 func (a *Agency) stop() {
 	wasStopped := a.stopped.Swap(true)
 	if wasStopped {
@@ -38,15 +39,18 @@ func (a *Agency) stop() {
 	a.betsFile.Close()
 }
 
+// Sets up the SIGTERM signal to stop the process
 func (a *Agency) setupStop() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGTERM)
 	go func() {
 		<-stop
 		a.stop()
+		os.Exit(0)
 	}()
 }
 
+// Reads a batch of bets from the csv file
 func (a *Agency) readBatch() []Bet {
 	bets := []Bet{}
 	if a.stopped.Load() {
@@ -71,6 +75,7 @@ func (a *Agency) readBatch() []Bet {
 	return bets
 }
 
+// Sends all of the bets from the file to the server in batchs.
 func (a *Agency) sendBets() {
 	total_sent := 0
 	result := "success"
@@ -93,6 +98,8 @@ func (a *Agency) sendBets() {
 	)
 }
 
+// Sends a request to the server to get the winner documents for this agency,
+// and logs the amount of winners.
 func (a *Agency) getWinners() {
 	message, _ := json.Marshal(GetWinnersMessage())
 	a.client.Send(string(message))
@@ -105,6 +112,7 @@ func (a *Agency) getWinners() {
 	log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %d", len(winners))
 }
 
+// Runs the Agency: sends all of the bets to the server and gets the winners.
 func (a *Agency) Run() {
 	a.setupStop()
 	a.client = NewClient(a.config)
